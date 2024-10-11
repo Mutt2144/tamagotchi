8d1ec79448ce687eabf1687a3542e44672fe90df
Mutt2144 <kayorichard58@gmail.com>
2024-10-11 20:39:45 -0300

    first

---
+++ Makefile
@@ -0,0 +1,6 @@
+CXX=g++
+SRC=src/main.cpp
+OUT=build/program
+
+all:
+	$(CXX) $(SRC) -o $(OUT)
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Tamagotchi!\n";
+
+    return 0;
+}
