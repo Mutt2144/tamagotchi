bac62c2911dccdfbe777ee12259d212b4d2d57f6
Mutt2144 <kayorichard58@gmail.com>
2024-10-12 15:34:03 -0300

    settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#define HEALTH_ATTRIB       0
-#define HUNGER_ATTRIB       1
-#define SLEEPINESS_ATTRIB   2
-#define HAPPINESS_ATTRIB    3
+#define NAME_ATTRIB         0
+#define HEALTH_ATTRIB       1
+#define HUNGER_ATTRIB       2
+#define SLEEPINESS_ATTRIB   3
+#define HAPPINESS_ATTRIB    4
 
 #define SPRITE_HAPPY        "assets/happy.gotchi"
 #define SPRITE_SAD          "assets/sad.gotchi"
@@ -10,24 +11,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <random>
+#include <ctime>
+
+const char* hello_message[2]  = { "Hello!", "Hi!" };
+const char* hungry_message[2] = { "I'm hungry.", "I'm very hungry, feed me!" };
+const char* sad_message[1]    = { "We can play something?" };
 
 struct TAMAGOTCHI_DATA {
+    std::string name;
     int health;
     int hunger;
     int sleepiness;
     int happiness;
+    int rage;
 };
 
 TAMAGOTCHI_DATA tamagotchi_data;
 
+void feed_tamagotchi() {
+    int value = rand() % 20;
+
+    tamagotchi_data.hunger -= value;
+    tamagotchi_data.happiness += 2;
+    
+    if (tamagotchi_data.hunger < 0) tamagotchi_data.hunger = 0;
+    if (tamagotchi_data.happiness > 100) tamagotchi_data.happiness = 10;
+}
+
+
 const char* get_face_id() {
-    if (tamagotchi_data.happiness > 60 && tamagotchi_data.health > 65) {
+    if (tamagotchi_data.happiness >= 50 && tamagotchi_data.health > 65) {
         return SPRITE_HAPPY;
+    }
+    if (tamagotchi_data.happiness < 50) {
+        return SPRITE_SAD;
+    }
+    if (tamagotchi_data.rage > 50) {
+        return SPRITE_ANGRY;
     }
 
     // TODO:
 
     return "";
+};
+
+void sent_message(const char* msg) {
+    std::cout << "(" << tamagotchi_data.name << "): " << msg << "\n";
+}
+
+void show_stats() {
+    std::cout << "Name:       " << tamagotchi_data.name << "\n";
+    std::cout << "health:     " << tamagotchi_data.health << "\n";
+    std::cout << "hunger:     " << tamagotchi_data.hunger << "\n";;
+    std::cout << "sleepiness: " << tamagotchi_data.sleepiness << "\n";
+    std::cout << "happiness:  " << tamagotchi_data.happiness << "\n";
 }
 
 void show_actions() {
@@ -72,6 +110,7 @@ void save_data() {
     std::ofstream file("./tamagotchi_data.txt");
 
     if (file.is_open()) {
+        file << tamagotchi_data.name << "\n";
         file << tamagotchi_data.health << "\n";
         file << tamagotchi_data.hunger << "\n";
         file << tamagotchi_data.sleepiness << "\n";
@@ -89,15 +128,24 @@ void load_data() {
     if (data.is_open()) {
         while (std::getline(data, line)) {
             if (line == "no_data") {
+                std::cout << "Type the name of your tamagotchi: ";
+                std::string tama_name;
+                //std::getline(std::cin, tama_name);
+
+
+                std::cout << "[DEBUG]" << tama_name << "\n\n";
+
+                tamagotchi_data.name       = tama_name;
                 tamagotchi_data.health     = 100;
                 tamagotchi_data.hunger     = 10;
                 tamagotchi_data.sleepiness = 100;
                 tamagotchi_data.happiness  = 100;
             } else {
-                if (line_count == 0) set_tamagotchi_attrib(HEALTH_ATTRIB,     std::stoi(line));
-                if (line_count == 1) set_tamagotchi_attrib(HUNGER_ATTRIB,     std::stoi(line));
-                if (line_count == 2) set_tamagotchi_attrib(SLEEPINESS_ATTRIB, std::stoi(line));
-                if (line_count == 3) set_tamagotchi_attrib(HAPPINESS_ATTRIB,  std::stoi(line));
+                if (line_count == 0) tamagotchi_data.name = line;
+                if (line_count == 1) set_tamagotchi_attrib(HEALTH_ATTRIB,     std::stoi(line));
+                if (line_count == 2) set_tamagotchi_attrib(HUNGER_ATTRIB,     std::stoi(line));
+                if (line_count == 3) set_tamagotchi_attrib(SLEEPINESS_ATTRIB, std::stoi(line));
+                if (line_count == 4) set_tamagotchi_attrib(HAPPINESS_ATTRIB,  std::stoi(line));
             }
 
             line_count++;
@@ -108,26 +156,48 @@ void load_data() {
 }
 
 void loop() {
+    int msg_id;
+    msg_id = rand() % 2;
+    draw_sprite(SPRITE_HAPPY);
+    sent_message(hello_message[msg_id]);
+    
     show_actions();
     std::string input;
+    std::cout << "> ";
 
     while (std::getline(std::cin, input)) {
         std::system("clear");
 
+        draw_sprite(SPRITE_HAPPY);
         show_actions();
+        std::cout << "> ";
+
+        if (input == "q" || input == "Q") {
+            save_data();
+            return;
+        }
+
+        if (input == "f" || input == "F") {
+            feed_tamagotchi();
+        }
+
+        if (input == "p" || input == "P") {
+        }
+
+        if (input == "s" || input == "S") {
+            show_stats();
+        }
     }
 }
 
 int main() {
+    srand(time(0));
+
     std::system("clear");
     load_data();
-    save_data();
-    std::cout << "health:     " << tamagotchi_data.health << "\n";
-    std::cout << "hunger:     " << tamagotchi_data.hunger << "\n";;
-    std::cout << "sleepiness: " << tamagotchi_data.sleepiness << "\n";
-    std::cout << "happiness:  " << tamagotchi_data.happiness << "\n";
 
     loop();
 
+    save_data();
     return 0;
 }
